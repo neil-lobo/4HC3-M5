7b200dd21fca3c0698827c29d28aca8d461eee85
Neil Lobo <neil_edward_lobo@hotmail.com>
2023-12-16 19:55:47 -0500

    searchbar

---
+++ src/lib/components/Searchbar.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+    import search from "$lib/icons/search.svg"
+    export let value: string;
+
+    // $: {
+    //     console.log(value);
+    // }
+</script>
+
+<section>
+    <img src={search} alt="search" width="30px">
+    <input type="text" placeholder="Search" bind:value={value}>
+</section>
+
+<style scoped lang="scss">
+section {
+    // padding: 5px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    border: black solid 2px;
+    border-radius: 7px;
+    width: 300px;
+    
+    img {
+        margin: 6px;
+    }
+
+    input {
+        font-size: 15px;
+        border: none;
+        background: none;
+        outline: none;
+        width: 80%;
+    }
+    
+}
+</style>
--- src/routes/(app)/servers/+page.svelte
+++ src/routes/(app)/servers/+page.svelte
@@ -1,26 +1,44 @@
-<script>
-	import Server from "$lib/components/Server.svelte";
+<script lang="ts">
+	import Searchbar from "$lib/components/Searchbar.svelte";
+    import Server from "$lib/components/Server.svelte";
 
     export let data;
+    let searchbar: string = "";
+
+    $: filteredServers = data.servers.filter(server => server.name.toLowerCase().startsWith(searchbar.trim().toLowerCase()));
 </script>
 
 <section>
+    <h1>Servers</h1>
+    <div class="filters">
+        <Searchbar bind:value={searchbar} />
+    </div>
     <div class="servers">
-        {#each data.servers as server}
-            <Server serverData={server}></Server>
+        {#each filteredServers as server}
+            <Server serverData={server}/>
             <br>
         {/each}
     </div>
 </section>
 
-<style scoped>
+<style scoped lang="scss">
 section {
     width: 100%;
     height: 100vh;
     display: flex;
     flex-direction: column;
-    justify-content: center;
     align-items: center;
+
+    h1 {
+        margin-top: 50px;
+    }
+}
+
+.filters {
+    width: 90%;
+    margin: 30px;
+    display: flex;
+    justify-content: flex-end;
 }
 
 .servers {
