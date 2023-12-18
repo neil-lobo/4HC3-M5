88d67eb94b53f8f20ab30c1ae18be3234149c367
Neil Lobo <neil_edward_lobo@hotmail.com>
2023-12-18 18:10:22 -0500

    community page

--- src/routes/(app)/community/[id]/+page.svelte
+++ src/routes/(app)/community/[id]/+page.svelte
@@ -1,1 +1,69 @@
-community
+<script>
+	import Server from '$lib/components/Server.svelte';
+
+	export let data;
+
+	let servers =
+		data.community?.serverIds.map((serverId) => {
+			return {
+				data: data.servers?.[serverId] ?? {
+					name: 'error',
+					logo: 'error',
+					description: 'error',
+					playerCount: 'error',
+					tags: []
+				},
+				id: serverId
+			};
+		}) ?? [];
+</script>
+
+<section>
+	{#if !data.error}
+		<h1>{data.community?.name}</h1>
+		<div class="about-container">
+			<h3>About:</h3>
+			<br />
+			{data.community?.description}
+		</div>
+		<div class="all-servers-continer">
+			<h3>Liked servers:</h3>
+			<br />
+			{#each servers as server}
+				<Server serverData={server.data} id={server.id} />
+				<br />
+			{/each}
+		</div>
+	{:else}
+		<div class="error">
+			<h1>Error!</h1>
+			{data.message}
+		</div>
+	{/if}
+</section>
+
+<style scoped lang="scss">
+	section {
+		padding: 50px;
+		width: 100%;
+		// border: red solid;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+
+		.about-container {
+			margin-top: 50px;
+			width: 70%;
+		}
+
+		.all-servers-continer {
+			margin-top: 50px;
+			width: 70%;
+		}
+	}
+
+	.error {
+		display: flex;
+		flex-direction: column;
+	}
+</style>
